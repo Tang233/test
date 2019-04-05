8d969ff14f05c8c142b1588d87b2b04bc16b829f
Tang233 <1825949538@qq.com>
2019-04-06 00:08:36 +0800

    文本bug

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg:'HelloWorld......'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
