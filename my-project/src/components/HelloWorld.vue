e0ed42d5308b355e4a8869f1c9458be0cb87f51f
Tang233 <1825949538@qq.com>
2019-04-08 23:49:53 +0800

    demo

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <p>{{msg}}</p>
+    <p>这里是rawhtml里面的代码：{{rawhtml}}</p>
+    <p>这里是加了rawhtml代码的标签效果：<span v-html="rawhtml"></span></p>
+    <p v-if="seen">你现在看得到我吗？</p>
+    <!--不是很懂v-bind-->
+    <p v-bind:id="dynamicId">1234</p>
+
 
   </div>
 </template>
@@ -10,7 +16,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg:'HelloWorld......'
+      msg: 'HelloWorld......',
+      rawhtml: '<span style= "color:red">red</span>',
+      seen: false
     }
   }
 }
